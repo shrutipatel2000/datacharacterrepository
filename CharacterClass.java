a9aba46ca81401d646f40b3128863f7011e27898
shruti patel <Shrutipatel203@gmail.com>
2023-10-31 19:37:49 +0000

    datacharacterrepository

---
+++ CharacterClass.java
@@ -0,0 +1,17 @@
+package Characterpackage;
+
+public class CharacterClass {
+
+    public static void main(String[] args){
+
+        char a=26;
+        char b=20;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
